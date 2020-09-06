a777013ad615a02057cf41a2733af5d54d020510
Alex Parry <51757193+alexparry@users.noreply.github.com>
2020-09-06 19:18:22 +0100

    Create hello-hills-road.cs

---
+++ hello-hills-road.cs
@@ -0,0 +1,13 @@
+using System;
+
+// Create a Hello Hills Road program
+namespace Hello
+{
+    class HelloHillsRoad
+    {
+        static void Main()
+        {
+            System.WriteLine("Hello Hills Road!");
+        }
+    }
+}
